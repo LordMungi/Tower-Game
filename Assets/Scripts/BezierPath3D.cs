bda31173e647370a19e4af7ebf5a0ffcac213bc9
LordMungi <lrdmungi@gmail.com>
2026-05-16 16:18:13 -0300

    Add GetPointInCurve to Bezier Path class

--- Assets/Scripts/BezierPath3D.cs
+++ Assets/Scripts/BezierPath3D.cs
@@ -4,39 +4,34 @@ public class BezierPath : MonoBehaviour
 {
     [SerializeField] private Transform[] ControlPoints;
 
+    private const float GIZMO_SIZE = 0.05f;
+
     private void OnDrawGizmos()
     {
         for (float t = 0; t <= 1; t += 0.05f)
         {
-            switch (ControlPoints.Length)
-            {
-                case 2:
-                    Gizmos.DrawSphere(ControlPoints[0].position + t * (ControlPoints[1].position - ControlPoints[0].position), 0.25f);
-                    break;
-
-                case 3:
-                    Gizmos.DrawSphere((1 - t) * ((1 - t) * ControlPoints[0].position + t * ControlPoints[1].position) + t * 
-                                     ((1 - t) * ControlPoints[1].position + t * ControlPoints[2].position), 0.25f);
-                    break;
-
-                case 4:
-                    Gizmos.DrawSphere(Mathf.Pow(1 - t, 3) * ControlPoints[0].position + 3 * Mathf.Pow(1 - t, 2) * t * ControlPoints[1].position +
-                                      3 * (1 - t) * Mathf.Pow(t, 2) * ControlPoints[2].position + Mathf.Pow(t, 3) * ControlPoints[3].position, 0.25f);
-                    break;
-                default:
-                    break;
-            }
+            Gizmos.DrawSphere(GetPointInCurve(t), GIZMO_SIZE);
         }
-
     }
 
-    void Start()
+    public Vector3 GetPointInCurve(float t)
     {
-    }
+        switch (ControlPoints.Length)
+        {
+            case 2:
+                return ControlPoints[0].position + t * (ControlPoints[1].position - ControlPoints[0].position);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            case 3:
+                return (1 - t) * ((1 - t) * ControlPoints[0].position + t * ControlPoints[1].position) + t *
+                ((1 - t) * ControlPoints[1].position + t * ControlPoints[2].position);
+
+            case 4:
+                return Mathf.Pow(1 - t, 3) * ControlPoints[0].position + 3 * Mathf.Pow(1 - t, 2) * t * ControlPoints[1].position +
+                3 * (1 - t) * Mathf.Pow(t, 2) * ControlPoints[2].position + Mathf.Pow(t, 3) * ControlPoints[3].position;
+
+            default:
+                break;
+        }
+        return Vector3.zero;
     }
 }
