6e31641d9f56ae5d41f0ccdb88cdf061593c05d2
LordMungi <lrdmungi@gmail.com>
2026-05-17 18:37:07 -0300

    Remove GameManager as singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,24 +3,9 @@ using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
-    private GameManager() { }
-    public static GameManager instance { get; private set; }
-
-
     [Header("Broadcast Events")]
     [SerializeField] private EventChannel BlockCreateEvent;
     [SerializeField] private EventChannel BlockDropEvent;
-
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-            Destroy(gameObject);
-    }
 
     void Start()
     {
