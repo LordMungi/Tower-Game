6c594cfedc34bba8042c114c9bcb33c49f596813
LordMungi <lrdmungi@gmail.com>
2026-05-18 21:19:18 -0300

    Add playerPrefs for audioMixers

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -19,6 +19,14 @@ public class SettingsManager : MonoBehaviour
             Destroy(gameObject);
     }
 
+    private void Start()
+    {
+        SetMixerFromPref("MasterVolume");
+        SetMixerFromPref("MusicVolume");
+        SetMixerFromPref("SFXVolume");
+        SetMixerFromPref("UIVolume");
+    }
+
     public float GetMixerValue(string group)
     {
         mixer.GetFloat(group, out float value);
@@ -51,5 +59,18 @@ public class SettingsManager : MonoBehaviour
             mixer.SetFloat(group, Mathf.Log10(Mathf.Clamp(value, 0f, 1f)) * 20);
         else 
             mixer.SetFloat(group, -144f);
+
+        PlayerPrefs.SetFloat("Audio" + group, value);
+        PlayerPrefs.Save();
+    }
+
+    private void SetMixerFromPref(string group)
+    {
+        float value = PlayerPrefs.GetFloat("Audio" + group);
+        if (value > 0)
+            mixer.SetFloat(group, Mathf.Log10(Mathf.Clamp(value, 0f, 1f)) * 20);
+        else
+            mixer.SetFloat(group, -144f);
+        Debug.Log(value);
     }
 }
